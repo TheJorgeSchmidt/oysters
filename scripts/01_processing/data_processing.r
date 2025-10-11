ba17e694ad0a58a571f1a8c90f5f9d3546dca7f5
Jorge Schmidt <git@jorgeschmidt.com>
2025-10-11 18:51:56 -0400

    added Florida-specific visualizations

--- scripts/01_processing/data_processing.r
+++ scripts/01_processing/data_processing.r
@@ -92,7 +92,7 @@ production_by_year_ep <- production_by_year |>
 
 production_by_year_ep
 
-# first visualization
+# first visualization - volume
 ggplot(
   data = production_by_year_ep,
   mapping = aes(x = year,
@@ -101,7 +101,7 @@ ggplot(
   ) +
   geom_point(alpha = 0.3) +
   labs(
-    title = "Landings of oysters in the U.S.",
+    title = "Landings of oysters in the U.S. (1950 - 2024)",
     subtitle = "Volumes in tons for Eastern and Pacific oysters",
     x = "Year",
     y = "Imperial Tons",
@@ -109,17 +109,104 @@ ggplot(
   ) +
   theme(
   legend.position = "bottom",
-  legend.justification = "center"
+  legend.justification = "center") +
+  scale_color_hue(labels = c("Eastern Oyster", "Pacific Oyster"))
+
+
+# second visualization - revenue
+ggplot(
+  data = production_by_year_ep,
+  mapping = aes(x = year,
+                y = total_dollars/1000,
+                color = species)
+  ) +
+  geom_point(alpha = 0.3) +
+  labs(
+    title = "Landings of oysters in the U.S. (1950 - 2024)",
+    subtitle = "Total revenues for Eastern and Pacific oysters",
+    x = "Year",
+    y = "Thousands of dollars",
+    color = "Species"
+  ) +
+  theme(
+  legend.position = "bottom",
+  legend.justification = "center") +
+  scale_color_hue(labels = c("Eastern Oyster", "Pacific Oyster"))
+
+# third visualization - revenue per pound
+ggplot(
+  data = production_by_year_ep,
+  mapping = aes(x = year,
+                y = (total_dollars/total_pounds),
+                color = species)
+  ) +
+  geom_point(alpha = 0.3) +
+  labs(
+    title = "Landings of oysters in the U.S. (1950 - 2024)",
+    subtitle = "Revenue per pound for Eastern and Pacific oysters",
+    x = "Year",
+    y = "Dollars per lb",
+    color = "Species"
+  ) +
+  theme(
+  legend.position = "bottom",
+  legend.justification = "center") +
+  scale_color_hue(labels = c("Eastern Oyster", "Pacific Oyster"))
+
+# fourth visualization - Florida volumes
+production_by_year_by_state <- us_landings_clean |>
+  group_by(year, state) |>
+  summarise(
+    total_pounds = sum(pounds, na.rm = TRUE),
+    total_dollars = sum(dollars, na.rm = TRUE)
   )
 
+# select only rows with observations for eastern and pacific oysters
+production_by_year_fl <- production_by_year_by_state |>
+  filter(grepl("FLORIDA", state))
 
+production_by_year_fl
 
-avg_annual_revenue <- mean(total_revenue$catch)
+# fifth visualize fl production - volumes
+ggplot(
+  data = production_by_year_fl,
+  mapping = aes(x = year,
+                y = total_pounds/1000,
+                color = state)
+  ) +
+  geom_point(alpha = 0.3) +
+  labs(
+    title = "Landings of oysters in Florida. (1950 - 2024)",
+    subtitle = "Volumes in tons for West and East coasts",
+    x = "Year",
+    y = "Imperial Tons",
+    color = "Coast"
+  ) +
+  theme(
+  legend.position = "bottom",
+  legend.justification = "center") +
+  scale_color_hue(labels = c("FL-East", "FL-West"))
 
-dollar(revenue, prefix = "$")
-dollar(avg_annual_revenue, prefix = "$")
+# sixth visualization - revenue per pound FL
+ggplot(
+  data = production_by_year_fl,
+  mapping = aes(x = year,
+                y = (total_dollars/total_pounds),
+                color = state)
+  ) +
+  geom_point(alpha = 0.3) +
+  labs(
+    title = "Landings of oysters in Florida (1950 - 2024)",
+    subtitle = "Revenue per pound for East and West coast oysters",
+    x = "Year",
+    y = "Dollars per lb",
+    color = "Coast"
+  ) +
+  theme(
+  legend.position = "bottom",
+  legend.justification = "center") +
+  scale_color_hue(labels = c("Fl - East", "FL - West"))
 
-## Another step ----------------------------------------------------------------
 
 ## Another step ----------------------------------------------------------------
 # calculate average price per lb per state per year
